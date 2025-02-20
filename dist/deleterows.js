688100f2ea4f4c0a0f272fccfdcdd6852ec14db7
Otso Saarinen <saarinenotso@gmail.com>
2025-02-20 18:14:19 +0200

    Update deleterows.js

--- dist/deleterows.js
+++ dist/deleterows.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-    (this && this.__importDefault) ||
-    function (mod) {
-        return mod && mod.__esModule ? mod : { default: mod };
-    };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sqlite3_1 = __importDefault(require("sqlite3"));
 const path_1 = __importDefault(require("path"));
@@ -13,22 +11,25 @@ const dbPath = path_1.default.resolve(__dirname, "../db/player_database.db");
 const db = new sqlite3_1.default.Database(dbPath, (err) => {
     if (err) {
         console.error("Failed to connect to the database:", err.message);
-    } else {
+    }
+    else {
         console.log(`Connected to the database at ${dbPath}`);
     }
 });
 // Correct DELETE statement
-db.run("DELETE FROM osu_players WHERE rank = 1", function (err) {
+db.run("DELETE FROM osu_players WHERE rank = 0", function (err) {
     if (err) {
         console.error("Error deleting from the database:", err);
-    } else {
+    }
+    else {
         console.log(`Deleted ${this.changes} rows from the database.`);
     }
     // Close the database after query
     db.close((closeErr) => {
         if (closeErr) {
             console.error("Error closing the database:", closeErr.message);
-        } else {
+        }
+        else {
             console.log("Database connection closed.");
         }
     });

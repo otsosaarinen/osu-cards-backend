f9baec318aced99d068db3521f6738dd9191c915
Otso Saarinen <saarinenotso@gmail.com>
2025-02-20 16:03:06 +0200

    Update deleterows.js

--- dist/deleterows.js
+++ dist/deleterows.js
@@ -1,26 +1,29 @@
-const sqlite3 = require("sqlite3").verbose();
-const path = require("path");
-
+"use strict";
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
+Object.defineProperty(exports, "__esModule", { value: true });
+const sqlite3_1 = __importDefault(require("sqlite3"));
+const path_1 = __importDefault(require("path"));
 // Database path (same as in your main script)
-const dbPath = path.resolve(__dirname, "../db/player_database.db");
-
-// Connect to the database
-const db = new sqlite3.Database(dbPath, (err) => {
+const dbPath = path_1.default.resolve(__dirname, "../db/player_database.db");
+// Create a new SQLite database connection
+const db = new sqlite3_1.default.Database(dbPath, (err) => {
     if (err) {
         console.error("Failed to connect to the database:", err.message);
     } else {
         console.log(`Connected to the database at ${dbPath}`);
     }
 });
-
 // Correct DELETE statement
-db.run("DELETE FROM osu_players WHERE rank = 0", function (err) {
+db.run("DELETE FROM osu_players WHERE rank = 1", function (err) {
     if (err) {
         console.error("Error deleting from the database:", err);
     } else {
         console.log(`Deleted ${this.changes} rows from the database.`);
     }
-
     // Close the database after query
     db.close((closeErr) => {
         if (closeErr) {

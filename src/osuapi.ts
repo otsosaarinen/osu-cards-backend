218c17db4e4e15746d876f790060bb2bd62a7d53
Otso Saarinen <saarinenotso@gmail.com>
2025-02-20 18:24:34 +0200

    Update osuapi.ts

--- src/osuapi.ts
+++ src/osuapi.ts
@@ -2,29 +2,38 @@ require("dotenv").config();
 
 const API_KEY = process.env.OSU_API_KEY;
 
-async function apiCall(username: string) {
-    const url = new URL("https://osu.ppy.sh/api/get_user");
+let player_list = ["Aetherian", "chu", "Tikkanen", "Haadez", "boleks"];
 
-    const params = { k: API_KEY, u: username, type: "string" };
-    Object.keys(params).forEach((key) =>
-        url.searchParams.append(key, (params as any)[key])
-    );
+let fetched: object[] = [];
 
-    try {
-        const response = await fetch(url.toString(), {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        });
+async function apiCall(username: string[]) {
+    for (let player of player_list) {
+        const url = new URL("https://osu.ppy.sh/api/get_user");
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+        const params = { k: API_KEY, u: player, type: "string" };
+        Object.keys(params).forEach((key) =>
+            url.searchParams.append(key, (params as any)[key])
+        );
+
+        try {
+            const response = await fetch(url.toString(), {
+                method: "GET",
+                headers: { "Content-Type": "application/json" },
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            fetched.push(data["username"]);
+        } catch (error) {
+            console.error("Error fetching osu! user data:", error);
         }
-
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error("Error fetching osu! user data:", error);
     }
 }
 
-apiCall("12311");
+apiCall(player_list);
+
+console.log(fetched);

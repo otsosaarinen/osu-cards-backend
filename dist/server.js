2a7d0aae24d82aac7f386cffd772ba07bc8eca59
Otso Saarinen <saarinenotso@gmail.com>
2025-02-21 11:46:36 +0200

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -1,1 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv").config();
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+app.get("/", (req, res) => {
+    res.send("Express + TypeScript server");
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});

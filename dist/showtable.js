7eedd8c1c372dc81b33b367e6ebe47b430813e0d
Otso Saarinen <saarinenotso@gmail.com>
2025-02-21 18:18:30 +0200

    Update showtable.js

--- dist/showtable.js
+++ dist/showtable.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-    (this && this.__importDefault) ||
-    function (mod) {
-        return mod && mod.__esModule ? mod : { default: mod };
-    };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sqlite3_1 = __importDefault(require("sqlite3"));
 const path_1 = __importDefault(require("path"));
@@ -11,21 +9,24 @@ const dbPath = path_1.default.resolve(__dirname, "../db/player_database.db");
 const db = new sqlite3_1.default.Database(dbPath, (err) => {
     if (err) {
         console.error("Failed to connect to the database:", err.message);
-    } else {
+    }
+    else {
         console.log(`Connected to the database at ${dbPath}`);
     }
 });
-db.all("SELECT * FROM osu_players", (err, rows) => {
+db.all("SELECT * FROM osu_players ORDER BY rank", (err, rows) => {
     if (err) {
         console.error("Error querying the database:", err);
-    } else {
+    }
+    else {
         console.log("All players in database:");
         console.table(rows);
     }
     db.close((closeErr) => {
         if (closeErr) {
             console.error("Error closing the database:", closeErr.message);
-        } else {
+        }
+        else {
             console.log("Database connection closed.");
         }
     });
